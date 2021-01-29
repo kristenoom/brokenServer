8227fb208a304d8cb04383b2f540ad029a1a14f9
Kayla Bullard <kaylabullard79@gmail.com>
2021-01-28 19:56:44 -0500

    first commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('brokenServer', 'postgres', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+
+sequelize.authenticate().then(
+    function success() {
+        console.log("Connected to DB");
+    },
+
+    function fail(err) {
+        console.log(`Error: ${err}`);
+    }
+)
